9bdf35010a5c026c225bd6308455dd6bb03f09b1
GUILHERME GUIMARÃES DE SOUSA <guilherme10007@gmail.com>
2026-08-17 20:31:55 -0300

    Objeto de Vetor

---
+++ AulasDaniel/ObjetosdeVetores/produto.js
@@ -0,0 +1,11 @@
+let produto = {
+    nome:"Mouse", 
+    descricao:"Mouse sem fio",
+    preco: 89.20, 
+    qtde: 3
+}
+
+
+console.log(produto)
+produto.preco = produto.preco * 0.90
+console.log(produto)
---
+++ AulasDaniel/ObjetosdeVetores/vetor.js
@@ -0,0 +1,11 @@
+let vetor = [
+    {filme: "A Odisseia", ano: 2026, genero:"Ficção"},
+    {filme:"Os Vingadores", ano: 2020, genero:"Ficção"},
+    {filme:"Home Aranha", ano:2026, genero:"Ficção"},
+]
+console.log(vetor)
+// Altera o vingadores para comédia
+vetor[1].genero ="comédia"
+// Altera Home Aranha para Homem-Aranha - Um novo dia
+vetor[2].filme = "Homem-Aranha - Um novo dia"
+console.log(vetor)  
