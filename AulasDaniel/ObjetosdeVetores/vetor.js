8199f7fdc702737aae249f4b47b79b767f93aa67
GUILHERME GUIMARÃES DE SOUSA <guilherme10007@gmail.com>
2026-08-18 21:51:52 -0300

    Objeto de vetor - Condiçoes

--- AulasDaniel/ObjetosdeVetores/vetor.js
+++ AulasDaniel/ObjetosdeVetores/vetor.js
@@ -5,7 +5,27 @@ let vetor = [
 ]
 console.log(vetor)
 // Altera o vingadores para comédia
-vetor[1].genero ="comédia"
+vetor[1].genero ="Comédia"
 // Altera Home Aranha para Homem-Aranha - Um novo dia
 vetor[2].filme = "Homem-Aranha - Um novo dia"
-console.log(vetor)  
+// Diminui o ano do filme Odisseia
+vetor[0].ano = vetor[0].ano -1
+console.log(vetor)
+
+for(let i=0; i<vetor.length;i++){
+    console.log(vetor[i].filme + " Genero: " + vetor[i].genero + " Ano: "+ vetor[i].ano)
+}
+let soma = 0
+for(let i=0; i<vetor.length;i++){
+soma = soma + vetor[i].ano 
+}
+let media = soma / vetor.length
+console.log("A média de ano dos filmes é " + media.toFixed(2))
+
+let busca = "A Odisseia"
+for(let i=0;i<vetor.length;i++){
+    if(vetor[i].filme === "A Odisseia"){
+    }
+    
+}
+
